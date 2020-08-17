8ffd5bcd9ddea213566c6a0cd1559a3c63c6e0cc
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-08-18 02:14:59 +0300

    create base variables for project

--- js/todo.js
+++ js/todo.js
@@ -1,1 +1,6 @@
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+let ul = document.querySelector(".list-group"),
+  form = document.forms["addTodoItem"],
+  inputText = form.elements["todoText"],
+  notificationAlert = document.querySelector(".notification-alert");

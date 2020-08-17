e5fe20640b31103e8c3dacf28b39739ae9c358dd
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-08-18 02:16:50 +0300

    create function for generate random ID

--- js/todo.js
+++ js/todo.js
@@ -4,3 +4,15 @@ let ul = document.querySelector(".list-group"),
   form = document.forms["addTodoItem"],
   inputText = form.elements["todoText"],
   notificationAlert = document.querySelector(".notification-alert");
+
+function generateId() {
+  let id = "";
+  let words = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+
+  for (let i = 0; i < 15; i++) {
+    let position = Math.floor(Math.random() * words.length);
+    id += words[position];
+  }
+
+  return id;
+}

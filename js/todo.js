94e523beffbf63ce28959e53e9acad42e1a3e95e
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-08-28 20:51:16 +0300

    realize delete item list for loop

--- js/todo.js
+++ js/todo.js
@@ -64,6 +64,15 @@ function addList(list) {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+function deleteListItem(id) {
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].id === id) {
+      tasks.splice(i, 1);
+      break;
+    }
+  }
+}
+
 function message(settings) {
   notificationAlert.classList.add(settings.cssClass);
   notificationAlert.textContent = settings.text;
